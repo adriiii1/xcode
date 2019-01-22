6d700f16efcaf84bd1c256d12171e12015053be4
DAMN <damn@Mac-mini-de-usuario.local>
2019-01-22 16:52:43 +0100

    WORKS, NO TOCAR

--- NavegadorSwift/ViewController.swift
+++ NavegadorSwift/ViewController.swift
@@ -7,15 +7,30 @@
 //
 
 import UIKit
+import SQLite3
 
 class ViewController: UIViewController {
 
+    var db: OpaquePointer?
+    
     @IBOutlet weak var web: UIWebView!
     @IBOutlet weak var url: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let fileURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+            .appendingPathComponent("Historial.sqlite")
         
+        if sqlite3_open(fileURL.path, &db) != SQLITE_OK {
+            print("Error al abrir la base de datos")
+        }
+        else {
+            print("Conectado")
+            if sqlite3_exec(db, "CREATE TABLE IF NOT EXISTS Historial (id INTEGER PRIMARY KEY AUTOINCREMENT, url TEXT)", nil, nil, nil) != SQLITE_OK {
+                let errmsg = String(cString: sqlite3_errmsg(db)!)
+                print("Error al crear la tabla: \(errmsg)")
+            }
+        }
     }
 
     @IBAction func buscar(_ sender: Any) {
@@ -33,6 +48,21 @@ class ViewController: UIViewController {
         let aString = url.text
         let newString = aString?.replacingOccurrences(of: " ", with: "+", options: .literal, range: nil)
         url.text = "https://google.com/search?q="+newString!
+        
+        var stmt: OpaquePointer?
+        let queryString = "INSERT INTO Historial (url) VALUES ('"+url.text!+"')"
+        if sqlite3_prepare(db, queryString, -1, &stmt, nil) != SQLITE_OK{
+            let errmsg = String(cString: sqlite3_errmsg(db)!)
+            print("Error al preparar SQL: \(errmsg)")
+            return
+        }
+        
+        if sqlite3_step(stmt) != SQLITE_DONE {
+            let errmsg = String(cString: sqlite3_errmsg(db)!)
+            print("Error al insertar: \(errmsg)")
+            return
+        }
+        
         web.loadRequest(URLRequest(url: URL(string: url.text!)!))
     }
 }
--- NavegadorSwift/ViewController2.swift
+++ NavegadorSwift/ViewController2.swift
@@ -7,24 +7,62 @@
 //
 
 import UIKit
+import SQLite3
 
-class ViewController2: UIViewController {
-
+class ViewController2: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var historial: [String] = []
+    var db: OpaquePointer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        historial.removeAll()
+        
+        let fileURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+            .appendingPathComponent("Historial.sqlite")
+        
+        if sqlite3_open(fileURL.path, &db) != SQLITE_OK {
+            print("Error al abrir la base de datos")
+        }
+        else {
+            print("Conectado")
+            if sqlite3_exec(db, "CREATE TABLE IF NOT EXISTS Historial (id INTEGER PRIMARY KEY AUTOINCREMENT, url TEXT)", nil, nil, nil) != SQLITE_OK {
+                let errmsg = String(cString: sqlite3_errmsg(db)!)
+                print("Error al crear la tabla: \(errmsg)")
+            }
+        }
+
+        let queryString = "SELECT * FROM Historial"
+  
+        var stmt:OpaquePointer?
+
+        if sqlite3_prepare(db, queryString, -1, &stmt, nil) != SQLITE_OK{
+            let errmsg = String(cString: sqlite3_errmsg(db)!)
+            print("Error al preparar SQL: \(errmsg)")
+            return
+        }
+
+        while(sqlite3_step(stmt) == SQLITE_ROW){
+            let url = String(cString: sqlite3_column_text(stmt, 1))
+    
+            historial.append(url)
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return historial.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell=UITableViewCell(style: UITableViewCell.CellStyle.subtitle, reuseIdentifier: "registro")
+        cell.textLabel?.text  = historial[indexPath.row]
+        return cell
+    }
+    @IBAction func salir(_ sender: Any) { self.view.window!.rootViewController?.dismiss(animated: true, completion: nil)
+    }
 }
